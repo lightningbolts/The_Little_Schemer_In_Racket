a51837b0350771e7f58118c0e41c325455bab7f0
lightningbolts <timberlake2025@gmail.com>
2020-10-08 19:22:54 -0700

    Oh my Gawd: It's full of stars: Part 2

--- Chapter_5.rkt
+++ Chapter_5.rkt
@@ -16,6 +16,60 @@
        [(eq? old (car lat)) (cons old (cons new (insertR* new old (cdr lat))))]
        [(cons (car lat) (insertR* new old (cdr lat)))])]))
 
+(define (occur* a l)
+  (cond
+    [(null? l) 0]
+    [(atom? (car l))
+     (cond
+       [(eq? (car l) a) (add1 (occur* a (cdr l)))]
+       [(occur* a (cdr l))]
+       )]
+    [(+ (occur* a (car l)) (occur* a (cdr l)))]))
+
+(define (subst* new old l)
+  (cond
+    [(null? l) '()]
+    [(not (list? (car l)))
+     (cond
+       [(eq? (car l) old)]
+       [(cons new (subst* new old (cdr l)))]
+       [(cons (car l) (subst* new old (cdr l)))])]
+    [(cons (subst* new old (car l)) (subst* new old (cdr l)))]))
+
+(define (member* a l)
+  (cond
+    [(null? l) #f]
+    [(not (list? (car l)))]
+    [(or (member* a (car l)) (member* a (cdr l)))]))
+
+(define (leftmost l)
+  (cond
+    [(not (list? (car l) (car l)))]
+    [(leftmost (car l))]))
+
+(define (eqlist? l1 l2)
+  (cond
+    [(and (null? l1) (null? l2)) #t]
+    [(or (null? l1) (null? l2)) #f]
+    [(and (equal? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2)))]))
+
+(define (equal? s1 s2)
+  (cond
+    [(and (atom? s1) (atom? s2))]
+    [(eqan? s1 s2)]
+    [(atom? s1) #f]
+    [(atom? s2) #f]
+    [(eqlist? s1 s2)]))
+
+(define (atom? a)
+  (not (list? a)))
+
+(define (eqan? a b)
+  (cond
+    [(and (number? a) (number? b)) (= a b)]
+    [(or (number? a) (number? b)) #f]
+    [(eq? a b)]))
+
 
                      
     
