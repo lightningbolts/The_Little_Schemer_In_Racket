70928f59e6e1bb8907e7f63e03dd28ec62377e26
lightningbolts <timberlake2025@gmail.com>
2020-10-17 19:17:41 -0700

    Fixed! Still to be continued...

--- Chapter_7.rkt
+++ Chapter_7.rkt
@@ -4,18 +4,18 @@
   (cond
     [(null? lat) #t]
     [(member? (car lat) (cdr lat)) #f]
-    [(set? (cdr lat))]))
+    [else (set? (cdr lat))]))
 
 (define (makeset lat)
   (cond
     [(null? lat) '()]
     [(member? (car lat) (cdr lat)) (makeset (cdr lat))]
-    [(cons (car lat) (makeset (cdr lat)))]))
+    [else (cons (car lat) (makeset (cdr lat)))]))
 
 (define (subset? set1 set2)
   (cond
     [(null? set1) #t]
-    [(and (member? (car set1) set2) (subset? (cdr set1) set2))]))
+    [else (and (member? (car set1) set2) (subset? (cdr set1) set2))]))
 
 (define (eqset? set1 set2)
     (and (subset? set1 set2) (subset? set2 set1)))
@@ -24,24 +24,24 @@
   (cond
     [(null? set1) #f]
     [(member? (car set1) set2) #t]
-    [(intersect? (cdr set1) set2)]))
+    [else (intersect? (cdr set1) set2)]))
 
 (define (intersect set1 set2)
   (cond
     [(null? set1) '()]
     [(member? (car set1) set2) (cons (car set1) (intersect (cdr set1) set2))]
-    [(intersect (cdr set1) set2)]))
+    [else (intersect (cdr set1) set2)]))
 
 (define (union set1 set2)
   (cond
     [(null? set1) set2]
     [(member? (car set1) (set2)) (union (cdr set1) (set2))]
-    [(cons (car set1) (union (cdr set1) set2))]))
+    [else (cons (car set1) (union (cdr set1) set2))]))
 
 (define (intersectall l-set)
   (cond
     [(null? (cdr l-set) (car l-set))]
-    [(intersect (car l-set) (intersectall (cdr l-set)))]))
+    [else (intersect (car l-set) (intersectall (cdr l-set)))]))
 
 (define (a-pair? x)
   (cond
@@ -49,12 +49,12 @@
     [(null? x) #f]
     [(null? (cdr x)) #f]
     [(null? (cdr (cdr x))) #f]
-    [#f]))
+    [else #f]))
 
 (define (revrel rel)
   (cond
     [(null? rel) '()]
-    [(cons (cons (car (cdr (car rel))) (cons (car (car rel)) '()))) (revrel (cdr rel))]))
+    [else (cons (cons (car (cdr (car rel))) (cons (car (car rel)) '()))) (revrel (cdr rel))]))
 
 (define (member? a b)
   (cond
